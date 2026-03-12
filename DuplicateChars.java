4c9266def7eec090937e7c0fa042686f377155cd
Sakshi Bhosale <sakshibhosale9673@gmail.com>
2026-03-12 10:37:55 +0530

    Add files via upload

---
+++ DuplicateChars.java
@@ -0,0 +1,16 @@
+package String_Practice;
+
+public class DuplicateChars {
+    public static void main(String[] args) {
+        String str = "programming";
+
+        for(int i = 0; i < str.length(); i++){
+            for(int j = i + 1; j < str.length(); j++){
+                if(str.charAt(i) == str.charAt(j)){
+                    System.out.println(str.charAt(i));
+                    break;
+                }
+            }
+        }
+    }
+}
