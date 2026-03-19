688c92fbffb5ef635b3e8409fb44ede74588521c
Sakshi Bhosale <sakshibhosale9673@gmail.com>
2026-03-19 11:07:02 +0530

    Add files via upload

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,25 +1,18 @@
-//Develop a program that prints the multiplication table for a given Number
 
-package Function_Loops;
+package Exercise_Practice;
 
 import java.util.Scanner;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number: ");
 
-        Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
 
-        System.out.println("Welcome to Multiplication World\n");
-        System.out.print("Please enter your number: ");
-        int num = sc.nextInt();
-
-        printMultiplicationTable(num);
+    for(int i = 1 ; i<=10; i++){
+       System.out.println(num + "x" + i + "=" + (num * i));
     }
-
-    public static void printMultiplicationTable(int num) {
-
-         for (int i = 1; i <= 10; i++) {
-            System.out.println(i * num);
-        }
+    sc.close();
     }
 }
