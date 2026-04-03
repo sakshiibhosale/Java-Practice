293eded41420b06bdef902433545bbe763d2e3d7
Sakshi Bhosale <sakshibhosale9673@gmail.com>
2026-04-03 22:26:19 +0530

    Add files via upload

---
+++ PalindromeCheck.java
@@ -0,0 +1,26 @@
+package String_Practice;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String str = "madam";
+
+        int left = 0;
+        int right = str.length() - 1;
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
